26d9e133e14b98460bd776cca333958093cfaa12
alirezat66 <alirezataghizadeh66@gmail.com>
2021-04-22 23:50:18 +0300

    complete configuration theme
    add google fonts packages and remove files,
    implement text theme based on style-guide

--- design/shortening_mobile_app/lib/main.dart
+++ design/shortening_mobile_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shortening_mobile_app/screen/home_screen.dart';
 
 import 'constant/constant_routes.dart';
@@ -20,6 +21,34 @@ class MyApp extends StatelessWidget {
         primaryColorDark:
             HSLColor.fromAHSL(1, 257, 0.27, 0.26).toColor(), //dark violet
         errorColor: HSLColor.fromAHSL(1, 0, 0.87, 0.67).toColor(), //red
+        textTheme: TextTheme(
+          headline1: GoogleFonts.poppins(
+              fontSize: 95, fontWeight: FontWeight.w500, letterSpacing: -1.5),
+          headline2: GoogleFonts.poppins(
+              fontSize: 61, fontWeight: FontWeight.w500, letterSpacing: -0.5),
+          headline3:
+              GoogleFonts.poppins(fontSize: 46, fontWeight: FontWeight.w400),
+          headline4: GoogleFonts.poppins(
+              fontSize: 35, fontWeight: FontWeight.w500, letterSpacing: 0.25),
+          headline5:
+              GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w500),
+          headline6: GoogleFonts.poppins(
+              fontSize: 22, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+          subtitle1: GoogleFonts.poppins(
+              fontSize: 18, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+          subtitle2: GoogleFonts.poppins(
+              fontSize: 16, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+          bodyText1: GoogleFonts.poppins(
+              fontSize: 18, fontWeight: FontWeight.w500, letterSpacing: 0.5),
+          bodyText2: GoogleFonts.poppins(
+              fontSize: 15, fontWeight: FontWeight.w500, letterSpacing: 0.25),
+          button: GoogleFonts.poppins(
+              fontSize: 15, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+          caption: GoogleFonts.poppins(
+              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.4),
+          overline: GoogleFonts.poppins(
+              fontSize: 13, fontWeight: FontWeight.w500, letterSpacing: 1.5),
+        ),
       ),
       initialRoute: Routes.homeScreen,
       routes: {
