dd6aac0fddcdc09f1a333fdd7b0480c57ea27080
alirezat66 <alirezataghizadeh66@gmail.com>
2021-04-22 21:56:49 +0300

    implement theme colors
    due to style-guide.md file, implement the theme data colors in ThemeData part and create constant_colors.dart for implement additional colors

---
+++ design/shortening_mobile_app/lib/constant/constant_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static Color grayishViolet = HSLColor.fromAHSL(1, 257, 0.07, 0.63).toColor();
+  static Color veryDarkBlue = HSLColor.fromAHSL(1, 255, 0.11, 0.22).toColor();
+  static Color veryDarkViolet = HSLColor.fromAHSL(1, 260, 0.08, 0.14).toColor();
+}
--- design/shortening_mobile_app/lib/main.dart
+++ design/shortening_mobile_app/lib/main.dart
@@ -14,16 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shortly',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: HSLColor.fromAHSL(1, 180, 0.66, 0.49).toColor(), // cyan
+        backgroundColor: HSLColor.fromAHSL(1, 0, 0, 0.75).toColor(), //gray
+        buttonColor: HSLColor.fromAHSL(1, 180, 0.66, 0.49).toColor(), // cyan
+        primaryColorDark:
+            HSLColor.fromAHSL(1, 257, 0.27, 0.26).toColor(), //dark violet
+        errorColor: HSLColor.fromAHSL(1, 0, 0.87, 0.67).toColor(), //red
       ),
       initialRoute: Routes.homeScreen,
       routes: {
